a389cb878c89ee888a5faa207d49ff7a38ec79c4
AnaBGarcia1603 <LunaBGitHub@outlook.com>
2023-08-07 20:38:07 -0300

    Cree claseUser

---
+++ src/ejercicio/pkg2/guia1parte1/User.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.pkg2.guia1parte1;
+
+/**
+ *
+ * @author Lunaira404
+ */
+public class User {
+    
+}
